ef3a98c7a7278e10b21e99c3b72d52853cbb7e69
xiolian <xiongjillian@yahoo.com>
2025-10-21 09:07:54 -0700

    Created tables for entities

---
+++ initialize.sql
@@ -0,0 +1,58 @@
+-- ENTITIES
+
+CREATE TABLE healthServices (
+    healthCategory VARCHAR(50) PRIMARY KEY,
+    healthBuilding VARCHAR(50) NOT NULL,
+    healthLocation VARCHAR(50) NOT NULL,
+    healthDate DATE NOT NULL,
+    healthTime TIME NOT NULL
+);
+
+CREATE TABLE funding (
+    fundingType VARCHAR(50) PRIMARY KEY,
+    fundingBuilding VARCHAR(50) NOT NULL,
+    fundingLocation VARCHAR(50) NOT NULL,
+    fundingDate DATE NOT NULL,
+    fundingTime TIME NOT NULL
+);
+
+CREATE TABLE student (
+    studentID INTEGER PRIMARY KEY AUTOINCREMENT,
+    studentName VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE academicAdvising (
+    advisorName VARCHAR(50) PRIMARY KEY,
+    advisingBuilding VARCHAR(50) NOT NULL,
+    advisingLocation VARCHAR(50) NOT NULL,
+    advisingDate DATE NOT NULL,
+    advisingTime TIME NOT NULL
+);
+
+CREATE TABLE academicSupport (
+    acaSuppName VARCHAR(50) PRIMARY KEY,
+    acaSuppBuilding VARCHAR(50) NOT NULL,
+    acaSuppLocation VARCHAR(50) NOT NULL,
+    acaSuppDate DATE NOT NULL,
+    acaSuppTime TIME NOT NULL
+);
+
+CREATE TABLE tutoring (
+    sessionID INTEGER PRIMARY KEY AUTOINCREMENT,
+    courseName VARCHAR(50) NOT NULL,
+    sessionDate DATE NOT NULL,
+    sessionTime TIME NOT NULL,
+    tutoringDepartment VARCHAR(50) NOT NULL,
+    tutoringBuilding VARCHAR(50) NOT NULL,
+    tutoringLocation VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE studentSupplies (
+    resourceType VARCHAR(50),
+    department VARCHAR(50),
+    stuSuppBuilding VARCHAR(50) NOT NULL,
+    stuSuppLocation VARCHAR(50) NOT NULL,
+    stuSuppDate DATE NOT NULL,
+    stuSuppTime TIME NOT NULL,
+    PRIMARY KEY (resourceType, department)
+);
