f1e1e3591ef7a57db9ab1b5c9dc9d70b2bdf3bf5
xiolian <xiongjillian@yahoo.com>
2025-11-29 19:49:54 -0800

    Added admin function that shows query of the counts of resources obtained by students

--- Databases/admin.py
+++ Databases/admin.py
@@ -1,13 +1,15 @@
 import sqlite3
 from sqlite3 import Error
-from databaseFuncs import database as dbc
+from databaseFuncs import databaseConn as dbc
+from databaseFuncs import adminFuncs as af
+
 def main():
     database = r"resource.sqlite"
 
     # create a database connection
     conn = dbc.openConnection(database)
-    # with conn:
-    #     Q1(conn)
+    with conn:
+        af.viewAllStudents(conn)
     #     Q2(conn)
     #     Q3(conn)
     #     Q4(conn)
---
+++ Databases/importantQueries.sql
@@ -0,0 +1,26 @@
+SELECT
+    s.studentID,
+    s.studentName,
+    COUNT(DISTINCT a.advisor_id) AS advisor_count,
+    COUNT(DISTINCT t.tutoring_id) AS tutoring_count,
+    COUNT(DISTINCT sup.supply_id) AS supplies_count,
+    COUNT(DISTINCT h.health_id) AS health_count,
+    COUNT(DISTINCT asupp.support_id) AS academic_support_count,
+    COUNT(DISTINCT f.funding_id) AS funding_count,
+    (
+      COUNT(DISTINCT a.advisor_id) +
+      COUNT(DISTINCT t.tutoring_id) +
+      COUNT(DISTINCT sup.supply_id) +
+      COUNT(DISTINCT h.health_id) +
+      COUNT(DISTINCT asupp.support_id) +
+      COUNT(DISTINCT f.funding_id)
+    ) AS total_resource_count
+
+FROM Student AS s, AdvisorRecord AS a, TutoringRecord AS t, StudentSuppliesRecord AS sup, HealthRecord AS h, FundingRecord AS f, AcademicSupportRecord AS asupp
+WHERE a.studentID   = s.studentID
+AND t.studentID   = s.studentID
+AND sup.studentID = s.studentID
+AND h.studentID   = s.studentID
+AND asupp.studentID = s.studentID
+AND f.studentID   = s.studentID
+GROUP BY s.studentID, s.studentName;
